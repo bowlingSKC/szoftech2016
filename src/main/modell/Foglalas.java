007d8bccb11c6d8e6318f0340c06a56524bf2f57
Lekszikov Kitti <Lekszikov Kitti@Kitti-PC>
2016-11-30 22:38:56 +0100

    Foglalás még kérdéses

--- src/main/modell/Foglalas.java
+++ src/main/modell/Foglalas.java
@@ -1,4 +1,18 @@
 package main.modell;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Foglalas {
+    
+    private ArrayList idopontok;
+
+    public Foglalas() {
+        idopontok = new ArrayList();
+    }
+    
+    
+    public void add(Date ido)
+    {
+        idopontok.add(ido);
+    }
 }

a317ee7076c8ac414b76714070e416ab4f3def9a
Lekszikov Kitti <Lekszikov Kitti@Kitti-PC>
2016-11-30 23:17:41 +0100

    Dátum forma hogyan legyen?

---
+++ src/main/kezelo/MegrendelesKezelo.java
@@ -0,0 +1,25 @@
+package main.kezelo;
+
+import java.util.List;
+
+public class MegrendelesKezelo extends Kezelo {
+
+    @Override
+    public void hozzaad() {
+        
+    }
+
+    @Override
+    public void torol() {
+    }
+
+    @Override
+    public void listaz() {
+    }
+
+    @Override
+    public List kereses() {
+        return null;
+    }
+    
+}

bfc72fffd3fb6268b77fc037187c8bdf96ee9fb8
Lekszikov Kitti <Lekszikov Kitti@Kitti-PC>
2016-12-02 16:57:57 +0100

    megrendeels osztaly kifejtese

--- src/main/modell/Megrendeles.java
+++ src/main/modell/Megrendeles.java
@@ -1,6 +1,7 @@
 package main.modell;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Megrendeles {
     
@@ -10,7 +11,8 @@ public class Megrendeles {
     private int koltseg;
     private ArrayList<Alkatresz> alkatreszek;
 
-    public Megrendeles(int azonosito, int partazon, int darab, int koltseg) {
+    //konstruktor úgy van megírva, hogy alkatrész listát is vár
+    public Megrendeles(int azonosito, int partazon, int darab, int koltseg, List<Alkatresz> alkatreszek) {
         this.azonosito = azonosito;
         this.partazon = partazon;
         this.darab = darab;
@@ -27,6 +29,22 @@ public class Megrendeles {
 
     public int getKoltseg() {
         return koltseg;
+    }
+
+    public void getMegrendeles()
+    {
+        System.out.println("Azonosito: " + azonosito);
+        System.out.println("Koltseg: " + koltseg);
+        System.out.println("Megrendelt alkatreszek:");
+        for(int i = 0; i < alkatreszek.size(); i++)
+        {
+            System.out.println(alkatreszek.get(i).getNev());
+            System.out.println(alkatreszek.get(i).getCikkszam());
+            System.out.println(alkatreszek.get(i).getMarka());
+            System.out.println(alkatreszek.get(i).getTipus());
+            System.out.println(alkatreszek.get(i).getBeszerzesiar());
+            System.out.println(alkatreszek.get(i).getEladasiAr());
+        }
     }
 
     

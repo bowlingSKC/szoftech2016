04cb88c1ef47a0baa37de8f202e6bd2dca52fa77
Lekszikov Kitti <Lekszikov Kitti@Kitti-PC>
2016-12-02 12:17:56 +0100

    megrendeles és kezeloje kovetkezik, illetve foglalasok kiegeszitese ora-perc-el

--- src/main/modell/Megrendeles.java
+++ src/main/modell/Megrendeles.java
@@ -1,17 +1,21 @@
 package main.modell;
 
+import java.util.ArrayList;
+
 public class Megrendeles {
     
     private int azonosito;
     private int partazon;
     private int darab;
     private int koltseg;
+    private ArrayList<Alkatresz> alkatreszek;
 
     public Megrendeles(int azonosito, int partazon, int darab, int koltseg) {
         this.azonosito = azonosito;
         this.partazon = partazon;
         this.darab = darab;
         this.koltseg = koltseg;
+        alkatreszek = new ArrayList();
     }
 
     

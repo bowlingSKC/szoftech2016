f9c1d953f6fc8759b880bbfbbfbf765661d3b344
Lekszikov Kitti <Lekszikov Kitti@Kitti-PC>
2016-12-02 17:10:51 +0100

    megrendeels osztaly kifejtese

--- src/main/kezelo/MegrendelesKezelo.java
+++ src/main/kezelo/MegrendelesKezelo.java
@@ -1,20 +1,33 @@
 package main.kezelo;
 
+import java.util.ArrayList;
 import java.util.List;
+import main.modell.Megrendeles;
 
 public class MegrendelesKezelo extends Kezelo {
 
+    ArrayList<Megrendeles> megrendelesek;
+    
     @Override
     public void hozzaad() {
+        System.out.println("=========Megrendeles felvetele=========");
         
     }
 
     @Override
     public void torol() {
+        
     }
 
     @Override
     public void listaz() {
+        
+        System.out.println("Folyamatban levo megrendelesek:");
+        for(int i = 0; i < megrendelesek.size(); i++)
+        {
+            megrendelesek.get(i).getMegrendeles();
+        }
+        
     }
 
     @Override

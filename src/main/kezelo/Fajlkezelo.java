1689dbcddf2967b015b0a6c8c2325df91e26cd5e
Hegyi Balázs József <h.bazsi940802@gmail.com>
2016-12-03 22:34:46 +0100

    fájlkezelő class létrehozva

---
+++ src/main/kezelo/Fajlkezelo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.kezelo;
+
+import java.io.BufferedReader;
+import main.SzervizMain;
+/**
+ *
+ * @author hbazs
+ */
+public class Fajlkezelo {
+    
+
+}
